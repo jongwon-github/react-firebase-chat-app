c95e2cef7420a3a859fbd8fdca8dac0b69283e28
jongwon <jwlee9939@gmail.com>
2023-01-16 10:02:37 +0900

    메시지 Body, 메시지 Form UI 만들기

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function MessageForm() {
-  return <div>MessageForm</div>;
+  return (
+    <div>
+      <Form.Control
+        as="textarea"
+        placeholder="Leave a comment here"
+        style={{ height: '100px' }}
+      />
+      <ProgressBar
+        variant="waring"
+        label="60%"
+        now={60}
+        style={{ marginTop: '10px' }}
+      />
+      <Row>
+        <Col>
+          <button className="message-form-button" style={{ width: '100%' }}>
+            SEND
+          </button>
+        </Col>
+        <Col>
+          <button className="message-form-button" style={{ width: '100%' }}>
+            UPLOAD
+          </button>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default MessageForm;
