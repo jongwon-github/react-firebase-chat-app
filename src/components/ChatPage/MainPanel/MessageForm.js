1198041ef6c5c895c993a5c2ca8dd356a423a590
jongwon <jwlee9939@gmail.com>
2023-01-19 14:41:35 +0900

    메세지 저장하기

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,16 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useSelector } from 'react-redux';
+import { getDatabase, ref, set, child, push } from 'firebase/database';
 
 function MessageForm() {
+  const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
+  const user = useSelector((state) => state.user.currentUser);
+  const [content, setContent] = useState('');
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const messagesRef = ref(getDatabase(), 'messages');
+
+  const createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: new Date(),
+      user: {
+        id: user.uid,
+        name: user.displayName,
+        image: user.photoURL,
+      },
+    };
+
+    if (fileUrl !== null) {
+      message['image'] = fileUrl;
+    } else {
+      message['content'] = content;
+    }
+
+    return message;
+  };
+
+  const handleSubmit = async () => {
+    console.log(createMessage());
+    if (!content) {
+      setErrors((prev) => prev.concat('Type contents first'));
+      return;
+    }
+    setLoading(true);
+    // firebase 에 메시지를 저정하는 부분
+    try {
+      console.log(createMessage());
+      await set(push(child(messagesRef, chatRoom.id)), createMessage());
+      setLoading(false);
+      setContent('');
+      setErrors([]);
+    } catch (error) {
+      setErrors((pre) => pre.concat(error.message));
+      setLoading(false);
+      setTimeout(() => {
+        setErrors([]);
+      }, 500);
+    }
+  };
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
   return (
     <div>
       <Form.Control
         as="textarea"
         placeholder="Leave a comment here"
         style={{ height: '100px' }}
+        value={content}
+        onChange={handleChange}
       />
       <ProgressBar
         variant="waring"
@@ -18,9 +75,20 @@ function MessageForm() {
         now={60}
         style={{ marginTop: '10px' }}
       />
+      <div>
+        {errors.map((errorMsg) => (
+          <p style={{ color: 'red' }} key={errorMsg}>
+            {errorMsg}
+          </p>
+        ))}
+      </div>
       <Row>
         <Col>
-          <button className="message-form-button" style={{ width: '100%' }}>
+          <button
+            className="message-form-button"
+            style={{ width: '100%' }}
+            onClick={handleSubmit}
+          >
             SEND
           </button>
         </Col>
